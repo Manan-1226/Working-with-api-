d38f75d53c4437beed35495658fcbff524adee65
Manan <mananrastogi1226@gmail.com>
2022-02-20 19:33:52 +0530

    First Commit

---
+++ Project 7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project 7
+//
+//  Created by Daffolapmac-155 on 11/02/22.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project 7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project 7
+//
+//  Created by Daffolapmac-155 on 11/02/22.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
